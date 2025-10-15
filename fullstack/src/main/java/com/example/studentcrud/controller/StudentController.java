381bf5c9bf489d6765227ae769a31601fab66ea9
Praveen-K-0503 <praveen0503k@gmail.com>
2025-10-15 13:01:22 +0530

    Add files via upload

---
+++ fullstack/src/main/java/com/example/studentcrud/controller/StudentController.java
@@ -0,0 +1,60 @@
+package com.example.studentcrud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.StudentRepository;
+import com.example.studentcrud.entity.Student;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+@Autowired
+private StudentRepository studentRepository;
+@PostMapping
+public Student CreateStudent(@RequestBody Student student)
+{
+	return studentRepository.save(student);
+}
+
+@GetMapping
+public List<Student>getAllstudents()
+{
+	return studentRepository.findAll();
+	
+}
+@GetMapping("/{id}")
+public Student getStudentBuId(@PathVariable Long id)
+{
+	return StudentRepository.findById(id).orElse(null);
+}
+@PutMapping("/{id}")
+public Student updateStudents(@PathVariable Long id,@RequestBody Student updatestudent)
+{
+	Student student = studentRepository.findById(id).orElse(null);
+	if(student!=null)
+	{
+		student.setName(updatestudent.getName());
+		student.setEmail(updatestudent.getEmail());
+		return studentRepository.save(student);
+	}
+	return null;
+}
+@DeleteMapping("/{id}")
+public String deleteStudent(@PathVariable Long id) {
+	studentRepository.deleteById(id);
+	return "Student deleted successfully!";
+	
+}
+
+}
---
+++ fullstack/src/main/java/com/example/studentcrud/entity/Student.java
@@ -0,0 +1,42 @@
+package com.example.studentcrud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String email;
+public Student() {
+	// TODO Auto-generated constructor stub
+}
+public Student(String name,String email) {
+	this.name=name;
+	this.email=email;
+	// TODO Auto-generated constructor stub
+}
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+
+}
