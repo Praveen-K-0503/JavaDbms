1e7af754e2413651f44fba583df5d4c1bf58555e
Praveen-K-0503 <praveen0503k@gmail.com>
2025-10-16 11:22:24 +0530

    Add files via upload

---
+++ junit-demo/src/test/java/com/example/junitdemo/BusinessServiceTests.java
@@ -0,0 +1,13 @@
+package com.example.junitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
